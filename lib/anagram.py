0df3aadfeab412facf9dd0222b620f32b58e12c0
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-05-28 01:14:52 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+# your code goes here!# your code goes here!
+
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        # Sort the letters of the initial word
+        sorted_word = sorted(self.word)
+        matches = []
+        
+        for word in word_list:
+            if sorted(word) == sorted_word:
+                matches.append(word)
+        
+        return matches
